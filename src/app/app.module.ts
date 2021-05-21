e3a98bcc7b1907d09d65418a87a2f6682916e875
Ben Halverson <benhalverson33@gmail.com>
2021-05-21 01:06:28 -0700

    refactor: 💡 Remove unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InputComponent } from './input/input.component';
-import { ItemComponent } from './item/item.component';
-import { TodoListService } from './todo-list.service';
-import { TodoListComponent } from './todo-list/todo-list.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
-import { MatIconModule } from '@angular/material/icon';
-import { FullCalendarModule } from '@fullcalendar/angular';
+
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListService } from './todo-list.service';
+
 @NgModule({
   declarations: [
     AppComponent,
-    InputComponent,
-    ItemComponent,
     TodoListComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    BrowserModule,
     FormsModule,
+    FullCalendarModule,
     MatDatepickerModule,
+    MatIconModule,
     MatInputModule,
     MatNativeDateModule,
     MatRippleModule,
-    MatIconModule,
-    FullCalendarModule,
   ],
   providers: [TodoListService],
   bootstrap: [AppComponent]
